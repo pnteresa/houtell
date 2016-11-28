1775f6871bf4e2f559b220d60131e166ed464ce0
Teresa Pranyoto <teresapranyoto@gmail.com>
2016-11-28 15:54:22 +0700

    added twig view render for city

--- src/public/index.php
+++ src/public/index.php
@@ -3,33 +3,54 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
+
 require '../lib/db.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('../templates', [
+        'cache' => false
+    ]);
+    
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
     $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
+    // $response->getBody()->write("Hello, $name");
+    return $this->view->render($response, 'index.html', [
+        'name' => $args['name']
+    ]);
 });
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Welcome!");
+$app->get('/', function (Request $request, Response $response, $args) {
+    return $this->view->render($response, 'index.html', []);
+})->setName('index');
 
-    return $response;
-});
+$app->get('/city/{id}-{nama_kota}', function (Request $request, Response $response, $args) {
+	$id = $args['id'];
+	$nama_kota = $args['nama_kota'];
+	$hotels = get_city_hotels($id, $nama_kota);
+    return $this->view->render($response, 'city.html', [
+    	'nama_kota' => $args['nama_kota'],
+    	'hotels' => $hotels
+    ]);
+})->setName('city');
 
-$app->get('/city/{id}-{nama_kota}', function (Request $request, Response $response) {
-	$id = $request->getAttribute('id');
-	$nama_kota = $request->getAttribute('nama_kota');
-	get_city_hotels($id, $nama_kota);
-});
-
-$app->get('/hotel/{id}+{canonical_name}', function (Request $request, Response $response) {
+$app->get('/hotel/{id}+{canonical_name}', function (Request $request, Response $response, $args) {
 	$id = $request->getAttribute('id');
 	$canonical_name = $request->getAttribute('canonical_name');
 	get_hotel($id);
-});
+})->setName('hotel');
 
 function get_city_hotels($id, $nama_kota) {
     $db = connect_db();
@@ -50,7 +71,7 @@ function get_city_hotels($id, $nama_kota) {
     	$data = array("error" => "invalid city");
     }
     $db = null;
-    echo json_encode($data);
+    return $data;
 }
 
 function get_hotel($id) {
