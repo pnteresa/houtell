5323e5c6b6939445b16c4baaf9be9e6480d6e665
Teresa Pranyoto <teresapranyoto@gmail.com>
2016-11-28 16:40:40 +0700

    added frontend from shylla

---
+++ src/public/static/js/script.js
@@ -0,0 +1,14 @@
+// -
+function expand() {
+  $(".search").toggleClass("close");
+  $(".input").toggleClass("square");
+  if ($('.search').hasClass('close')) {
+    $('input').focus();
+  } else {
+    $('input').blur();
+  }
+}
+
+$('button').on('click', expand);
+
+copyright('');
